5a3d519c85d89b4bf7da7ff687eafcc5f5b34761
Diogo <diogofilipebotelho@gmail.com>
2021-08-31 19:46:02 -0400

    did part I

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+"use strict";
+
+const GIPHY_URL = "http://api.giphy.com/v1/gifs/search?q=";
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+
+$(".search-form").on("submit", getGiphy);
+
+async function getGiphy(evt) {
+    evt.preventDefault();
+    let searchTerm = $("#search-term").val(); 
+    let response = await axios.get(`${GIPHY_URL}${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    console.log("response=",response);
+    console.log("response.data=".response.data)
+}
+
 console.log("Let's get this party started!");
+
+//{}
