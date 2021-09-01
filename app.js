e119495ae90c7af99b8be0fecf4160caaa9074d0
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-08-31 18:03:00 -0700

    refactored js code

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 "use strict";
 
-const GIPHY_URL = "http://api.giphy.com/v1/gifs/search?q=";
-// const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+const GIPHY_URL = "http://api.giphy.com/v1/gifs/search?";
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
 
 $(".search-form").on("submit", getGiphy);
 
+/** Uses Ajax to get a random image url and append it to the giphy-gallery */
 async function getGiphy(evt) {
     evt.preventDefault();
     let searchTerm = $("#search-term").val(); 
-    let response = await axios.get(`${GIPHY_URL}${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    let response = await axios.get(GIPHY_URL, { params:{q: searchTerm, "api_key": API_KEY}} );
+    // `${GIPHY_URL}${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
     // console.log("response=",response);
     // console.log("response.data=", response.data)
     // console.log("response.data.data:", response.data.data);
-    let index = getRandomNumber();
+    let responseLength = response.data.data.length;
+    let index = getRandomNumber(responseLength);
     let newGiphyURL = response.data.data[index].images.fixed_width.url;
     // console.log("newGiphyURL:", newGiphyURL);
     appendGiphy(newGiphyURL);
@@ -25,16 +28,19 @@ async function getGiphy(evt) {
 
 $("#remove-giphies-button").on("click", removeGiphies)
 
+/** removes all giphy images from the giphy-gallery div */
 function removeGiphies() {
     $(".giphy-gallery").empty();
 }
 
 // console.log("Let's get this party started!");
 
-function getRandomNumber () {
-    return Math.floor(Math.random()*50);
+/** using the responseLength, returns a random number between 0 and the responseLength */
+function getRandomNumber (responseLength) {
+    return Math.floor(Math.random()*responseLength);
 }
 
+/** creates a new img tag and adds the src and alt attributes, it then appends this new img tag to the giphy-galler */
 function appendGiphy (newGiphyURL) {
     let newGiphyHtml = $("<img>").attr("src", newGiphyURL).attr("alt", "giphy");
     // console.log("newGiphyHtml:", newGiphyHtml);
