c8d4c2204ed16dddac29897679363b9f07a4926e
Diogo <diogofilipebotelho@gmail.com>
2021-08-31 20:21:44 -0400

    finished exercise

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const GIPHY_URL = "http://api.giphy.com/v1/gifs/search?q=";
-const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+// const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
 
 $(".search-form").on("submit", getGiphy);
 
@@ -9,16 +9,27 @@ async function getGiphy(evt) {
     evt.preventDefault();
     let searchTerm = $("#search-term").val(); 
     let response = await axios.get(`${GIPHY_URL}${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-    console.log("response=",response);
-    console.log("response.data=", response.data)
-    console.log("response.data.data:", response.data.data);
+    // console.log("response=",response);
+    // console.log("response.data=", response.data)
+    // console.log("response.data.data:", response.data.data);
     let index = getRandomNumber();
     let newGiphyURL = response.data.data[index].images.fixed_width.url;
-    console.log("newGiphyURL:", newGiphyURL);
+    // console.log("newGiphyURL:", newGiphyURL);
     appendGiphy(newGiphyURL);
 }
 
-console.log("Let's get this party started!");
+/**Remove Function
+ * event listener on the button
+ * clears the div using .empty()
+ */
+
+$("#remove-giphies-button").on("click", removeGiphies)
+
+function removeGiphies() {
+    $(".giphy-gallery").empty();
+}
+
+// console.log("Let's get this party started!");
 
 function getRandomNumber () {
     return Math.floor(Math.random()*50);
@@ -26,6 +37,6 @@ function getRandomNumber () {
 
 function appendGiphy (newGiphyURL) {
     let newGiphyHtml = $("<img>").attr("src", newGiphyURL).attr("alt", "giphy");
-    console.log("newGiphyHtml:", newGiphyHtml);
+    // console.log("newGiphyHtml:", newGiphyHtml);
     $(".giphy-gallery").append(newGiphyHtml);
 }
