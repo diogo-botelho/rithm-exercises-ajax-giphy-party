02140aefc82c866e9946bd288d20697c9e13ccf5
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-08-31 17:14:43 -0700

    finished part 2

--- app.js
+++ app.js
@@ -10,9 +10,22 @@ async function getGiphy(evt) {
     let searchTerm = $("#search-term").val(); 
     let response = await axios.get(`${GIPHY_URL}${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
     console.log("response=",response);
-    console.log("response.data=".response.data)
+    console.log("response.data=", response.data)
+    console.log("response.data.data:", response.data.data);
+    let index = getRandomNumber();
+    let newGiphyURL = response.data.data[index].images.fixed_width.url;
+    console.log("newGiphyURL:", newGiphyURL);
+    appendGiphy(newGiphyURL);
 }
 
 console.log("Let's get this party started!");
 
-//{}
+function getRandomNumber () {
+    return Math.floor(Math.random()*50);
+}
+
+function appendGiphy (newGiphyURL) {
+    let newGiphyHtml = $("<img>").attr("src", newGiphyURL).attr("alt", "giphy");
+    console.log("newGiphyHtml:", newGiphyHtml);
+    $(".giphy-gallery").append(newGiphyHtml);
+}
